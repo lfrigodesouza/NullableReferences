7373496d1bd74c876a3d9874941cfb2922198447
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2019-10-30 21:47:04 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace NullableReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "José";
+            int contagem = nome.Length;
+            string textoVazio = string.Empty;
+            contagem = textoVazio.Length;
+            string? textoNulo = null;
+            if (!string.IsNullOrEmpty(textoNulo))
+                contagem = textoNulo.Length;
+            string? outroTextoNulo = RetornaTextoNulo();
+            if (!string.IsNullOrEmpty(outroTextoNulo))
+                contagem = outroTextoNulo.Length;
+        }
+
+        public static string? RetornaTextoNulo()
+        {
+            string? textoVazio = null;
+            return textoVazio;
+        }
+    }
+}
